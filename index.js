e7fd2bf5a0812d4c570adf70a03b1380002dd852
aaryanmehta2307 <aaryanmehta619@gmail.com>
2024-10-10 19:22:03 +0530

    Form-added-with-autosave

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express=require("express");
+const express = require("express");
+const app = express();
 
-const app=express();
-app.get("/",(req,res)=>{
-    res.send("<h2>Hi There I am aryan Mehta I love soo much </h2>");
+// Serve static files like CSS
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + ".public/index.html");
 });
-const port =process.env.PORT || 3000;
 
-app.listen(port,()=>console.log(`Listening on port ${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
